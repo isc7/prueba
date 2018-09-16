1415e217fd07ec637f185944cbd0c7787fd7b0db
ivan <ivan@ivan-PC>
2018-09-16 19:08:52 +0200

    cambio 2
    Change-Id: I1843496d73608ccd3b39bdd23f3782f1597bd54a

--- tutorial/src/main/java/com/programandoointantandolo/tutorial/TutorialApplication.java
+++ tutorial/src/main/java/com/programandoointantandolo/tutorial/TutorialApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TutorialApplication {
 
+	/**
+	 * 2
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
 	}
